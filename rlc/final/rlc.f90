e2b51978ca13f4fa235073eee2b07d7b02b99323
Alan <GeeWhyAlan@outlook.com>
2024-06-14 19:12:23 -0500

    fix rlc driving voltage

--- rlc/final/rlc.f90
+++ rlc/final/rlc.f90
@@ -1,7 +1,7 @@
 program rlc
   implicit none
 
-  integer, parameter :: max_n = 12000
+  integer, parameter :: max_n = 5000
   integer :: i
 
   real(8) :: q_0, C
@@ -14,7 +14,8 @@ program rlc
   real(8) :: c_i(max_n) ! current current in circuit
 
   real(8) :: E_0 ! driving voltage
-  real(8) :: w_0 ! resonant frequency
+  !real(8) :: w_0 ! resonant frequency
+  real(8) :: w_d ! driving frequency
 
   real(8), parameter :: step = 0.01d0
   real(8), parameter :: domain = 25.d0
@@ -24,21 +25,24 @@ program rlc
   
   print *, "Enter capacitor q_0:"
   read(*,*) q_0
-  print *, "Enter capacitor C:"
+  print *, "Enter capacitance C:"
   read(*,*) C
 
-  print *, "Enter inductor L:"
+  print *, "Enter inductance L:"
   read(*,*) L
   
   print *, "Enter i_0:"
   read(*,*) i_0
-  print *, "Enter resistor R:"
+  print *, "Enter resistance R:"
   read(*,*) R
 
   print *, "Enter driving voltage:"
   read(*,*) E_0
 
-  w_0 = 1.d0/sqrt(L*C)
+  !w_0 = 1.d0/sqrt(L*C)
+  print *, "Enter driving frequency:"
+  read(*,*) w_d
+
 
   open(unit=100, file="q_t.dat")
   open(unit=120, file="i_t.dat")
@@ -50,7 +54,7 @@ program rlc
 
   c_i(1) = i_0
   c_q(1) = q_0
-  d_i(1) = get_di(c_t, c_q(1), c_i(1), L, E_0, w_0, R, C)
+  d_i(1) = get_di(c_t, c_q(1), c_i(1), L, E_0, w_d, R, C)
   c_t = c_t + step
   c_i(2) = d_i(1)*step
   c_q(2) = c_q(1) + step*c_i(1) + (0.5)*d_i(1)*(step)**(2)
@@ -58,7 +62,7 @@ program rlc
   
   do i = 2, max_n - 1 
    
-    d_i(i) = get_di(c_t, c_q(i), c_i(i), L, E_0, w_0, R, C)
+    d_i(i) = get_di(c_t, c_q(i), c_i(i), L, E_0, w_d, R, C)
     c_q(i+1) = 2.d0*c_q(i) - c_q(i-1) + d_i(i)*(step)**(2)
     c_i(i+1) = (c_q(i+1) - c_q(i-1))/(2.d0*step)
 
@@ -85,12 +89,11 @@ program rlc
 
 contains
 
-function get_di(t, c_q, c_i, L, E_0, w_0, R, C) result(d_i)
-  real(8), intent(in) :: t, c_q, c_i, L, E_0, w_0, R, C
+function get_di(t, c_q, c_i, L, E_0, w_d, R, C) result(d_i)
+  real(8), intent(in) :: t, c_q, c_i, L, E_0, w_d, R, C
   real(8)             :: d_i
 
-  d_i = (1/L) * (E_0*sin(w_0*t) - R*c_i - c_q/C)
-  !d_i = -(1/L) * (R*c_i + c_q/C)
+  d_i = (E_0*cos(w_d*t) - R*c_i - c_q/C) / L
 
 end function
 
