1b662d4c0bf54d8f6421449bc29178f6510e35bc
Alan <GeeWhyAlan@outlook.com>
2024-06-16 22:51:37 -0500

    add naive rk4 orbital solution

--- orbital/naive/orbit.f90
+++ orbital/naive/orbit.f90
@@ -8,35 +8,62 @@ program orbit
     c_r = (/1.d0, 0.d0, 0.d0/),    &
     c_v = (/0.d0, 2.d0*pi, 0.d0/), &
     c_a = (/0.d0, 0.d0, 0.d0/)
-  
-  real(8) :: k_1, k_2, k_3, k_4 ! for RK4
 
-  real(8), parameter :: step = 0.01d0
+  real(8), dimension(4, 3) :: k ! for RK4
+
+  real(8) :: ke, gpe
+
+  real(8), parameter :: step = 1.d-4
   real(8) :: c_t = 0
 
-  real(8) :: domain = 100
+  real(8) :: domain = 1 !yr
+
+  open(unit=100, file="r_t.dat")
+  open(unit=110, file="k_t.dat")
+  open(unit=120, file="u_t.dat")
+  open(unit=130, file="e_t.dat")
 
   do while (c_t < domain)
     
-    k_1 = c_v
-    k_2 = c_v + step*get_a(c_r+k_1*step/2.d0)
-    k_3 = c_v + step*get_a(c_r+k_2*step/2.d0)
-    k_4 = c_v + step*get_a(c_r+k_3*step)
+    k(1,:) = c_v
+    k(2,:) = c_v + (step/2.d0)*get_a(c_r+k(1,:)*step/2.d0)
+    k(3,:) = c_v + (step/2.d0)*get_a(c_r+k(2,:)*step/2.d0)
+    k(4,:) = c_v + step*get_a(c_r+k(3,:)*step)
 
-    c_r = c_r + step/6.d0 * (k_1+k_2+k_3+k_4)
+    c_r = c_r + (step/6.d0) * (k(1,:)+2.d0*k(2,:)+2.d0*k(3,:)+k(4,:))
+    
+    c_v = c_v + get_a(c_r)*step
+    !ke = (0.5d0)*GM*sqrt(c_v(1)**2 + c_v(2)**2 + c_v(3)**2)
 
-    c_v = c_v + get_a(c_x)*step
+    write(100,*) c_r(1), c_r(2), c_r(3)  
+    !write(110,*) c_t, ke
+    !write(120,*) c_t, gpe
+    !write(130,*) c_t, ke+gpe
+
 
     c_t = c_t + step
   end do
 
+  close(100)
+  close(110)
+  close(120)
+  close(130)
+
 contains
+
+function get_magnitude(vec) result(sca)
+  real(8), dimension(3), intent(in) :: vec
+  real(8)                           :: sca
+
+  sca = sqrt(vec(1)**2.d0 + vec(2)**2.d0 + vec(3)**2.d0)
+
+end function
 
 function get_a(r) result(a)
   real(8), dimension(3), intent(in) :: r
   real(8), dimension(3)             :: a
   
-  a = GM/(r**2)
+  a = -GM*r/(get_magnitude(r)**3.d0)
 
 end function
 
