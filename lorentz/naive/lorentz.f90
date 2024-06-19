bb9abcb323b9b68bf7be0a46c0e6e9b32808a363
Alan <GeeWhyAlan@outlook.com>
2024-06-19 11:17:39 -0500

    fix lorentz

--- lorentz/naive/lorentz.f90
+++ lorentz/naive/lorentz.f90
@@ -6,16 +6,16 @@ program lorentz
     m = 1.d0    !kg
 
   real(8), dimension(3), parameter :: &
-    E = (/0.d0, 0.01d0, 0.1d0/), & !N/C
-    B = (/0.d0, 0.d0, 0.1d0/)      !T    
+    E = [0.d0, 0.01d0, 0.1d0], & !N/C
+    B = [0.d0, 0.d0, 0.1d0]      !T    
 
-  real(8), dimension(3) ::      &
-    c_r = (/0.d0, 0.d0, 0.d0/), &
-    c_v = (/1.d0, 0.d0, 0.d0/)
- 
-  real(8), dimension(4, 3) :: k ! for RK4
+  ! x y z vx vy vz
+  real(8), dimension(6) :: state = [0.d0, 0.d0, 0.d0, 1.d0, 0.d0, 0.d0]
+  
+  real(8), dimension(4, 6) :: k ! for RK4
+  real(8), dimension(6) :: temp
 
-  real(8), parameter :: domain = 500
+  real(8), parameter :: domain = 1000
   real(8), parameter :: step = 0.1d0
   
   real(8) :: c_t = 0
@@ -24,16 +24,27 @@ program lorentz
   
   do while (c_t < domain)
     
-    k(1,:) = c_v
-    k(2,:) = c_v + (step/2.d0) * get_a(c_v+k(1,:)*step/2.d0)
-    k(3,:) = c_v + (step/2.d0) * get_a(c_v+k(2,:)*step/2.d0)
-    k(4,:) = c_v + step * get_a(c_v+k(3,:)*step)
+    k(1,1:3) = state(4:6)
+    k(1,4:6) = get_a(state(4:6))
 
-    c_r = c_r + (step/6.d0) * (k(1,:)+2.d0*k(2,:)+2.d0*k(3,:)+k(4,:))
+    temp = state + (0.5)*step*k(1,:)
     
-    c_v = c_v + get_a(c_v)*step
+    k(2,1:3) = temp(4:6)
+    k(2,4:6) = get_a(temp(4:6))
 
-    write(100,*) c_r(1), c_r(2), c_r(3)
+    temp = state + (0.5)*step*k(2,:)
+    
+    k(3,1:3) = temp(4:6)
+    k(3,4:6) = get_a(temp(4:6))
+
+    temp = state + step*k(3,:)
+    
+    k(4,1:3) = temp(4:6)
+    k(4,4:6) = get_a(temp(4:6))
+
+    state = state + (step/6.d0) * (k(1,:)+2.d0*k(2,:)+2.d0*k(3,:)+k(4,:))
+
+    write(100,*) state(1), state(2), state(3)
 
     c_t = c_t + step
   end do
