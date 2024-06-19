6b473e86270efdda9f0d136b5f5b33510f9966a2
Alan <GeeWhyAlan@outlook.com>
2024-06-19 11:00:33 -0500

    fix rk4 algorithm

--- orbital/final/orbit.f90
+++ orbital/final/orbit.f90
@@ -5,11 +5,11 @@ program orbit
   real(8), parameter :: GM = 4.d0*(pi)**(2) ! AU^3/yr^2
   real(8), parameter :: mass = 1.d0
 
-  real(8), dimension(3) ::         &
-    c_r = [1.d0, 0.d0, 0.d0],      &
-    c_v = [0.d0, 2.d0*pi, 0.d0]
-
-  real(8), dimension(4, 3) :: k ! for RK4
+  ! x y z vx vy vz
+  real(8), dimension(6) :: state = [1.d0, 0.d0, 0.d0, 0.d0, 2.d0*pi, 0.d0]
+  
+  real(8), dimension(4, 6) :: k ! for RK4
+  real(8), dimension(6) :: temp
 
   real(8) :: ke, u
 
@@ -24,20 +24,31 @@ program orbit
   open(unit=130, file="e_t.dat")
 
   do while (c_t < domain)
+   
+    k(1,1:3) = state(4:6)
+    k(1,4:6) = get_a(state)
+
+    temp = state + (0.5)*step*k(1,:)
     
-    k(1,:) = c_v
-    k(2,:) = c_v + (step/2.d0) * get_a(c_r+k(1,:)*step/2.d0)
-    k(3,:) = c_v + (step/2.d0) * get_a(c_r+k(2,:)*step/2.d0)
-    k(4,:) = c_v + step * get_a(c_r+k(3,:)*step)
+    k(2,1:3) = temp(4:6)
+    k(2,4:6) = get_a(temp(1:3))
 
-    c_r = c_r + (step/6.d0) * (k(1,:)+2.d0*k(2,:)+2.d0*k(3,:)+k(4,:))
+    temp = state + (0.5)*step*k(2,:)
     
-    c_v = c_v + get_a(c_r)*step
+    k(3,1:3) = temp(4:6)
+    k(3,4:6) = get_a(temp(1:3))
 
-    ke = (0.5d0) * mass * norm2(c_v)**2
-    u = - mass * GM / norm2(c_r)
+    temp = state + step*k(3,:)
+    
+    k(4,1:3) = temp(4:6)
+    k(4,4:6) = get_a(temp(1:3))
 
-    write(100,*) c_r(1), c_r(2), c_r(3)  
+    state = state + (step/6.d0) * (k(1,:)+2.d0*k(2,:)+2.d0*k(3,:)+k(4,:))
+    
+    ke = (0.5d0) * mass * norm2(state(4:6))**2
+    u = - mass * GM / norm2(state(1:3))
+
+    write(100,*) state(1), state(2), state(3)  
     write(110,*) c_t, ke
     write(120,*) c_t, u
     write(130,*) c_t, ke+u
